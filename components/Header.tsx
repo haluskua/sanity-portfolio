56f4f8ac0aaed10c222e45231789f11525b673c8
haluskua <haluskua.one@gmail.com>
2022-11-17 21:53:35 +1300

    added the header with framer motion effect

---
+++ components/Header.tsx
@@ -0,0 +1,50 @@
+import { motion } from "framer-motion";
+import React, { ReactElement } from "react";
+import { SocialIcon } from "react-social-icons";
+
+interface Props {}
+
+export default function Header({}: Props): ReactElement {
+  return (
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-25 xl:items-center p-5">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.4 }}
+        className="flex flex-row items-center"
+      >
+        {/* Social Icons*/}
+        <SocialIcon
+          url="https://www.linkedin.com/in/haluskua-77/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/haluskua-77/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/haluskua-77/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+      <motion.div
+        initial={{ x: 500, opacity: 0, scale: 0 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-grey-400">
+          Get In Touch
+        </p>
+      </motion.div>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -7,9 +8,14 @@ const Home: NextPage = () => {
       <Head>
         <title>Gunter's Portfolio</title>
       </Head>
-      <main>
-        <h1 className="text-blue-800">Web Portfolio</h1>
-      </main>
+      <main>{/* <h1 className="text-blue-800">Web Portfolio</h1> */}</main>
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+      {/* About */}
+      {/* Experience */}
+      {/* Skills */}
+      {/* Contact Me */}
     </div>
   );
 };
