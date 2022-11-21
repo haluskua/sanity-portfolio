180a018dafeaeeda3badd5c7a970726d3b4fac1c
haluskua <haluskua.one@gmail.com>
2022-11-21 22:00:58 +1300

    added profile image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,24 +1,35 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircle from "./BackgroundCircle";
+import Image from "next/image";
+import ProfilePic from "../public/images/0.jpg";
 
 type Props = {};
 
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
     words: [
-      "Hi, The name's Gunter",
-      "The Guy-who-loves-Bubble-tea",
+      "Hi, The name's Ompa",
+      "The Guy-who-works-Construction",
       "<ButLovesToCode />",
     ],
     loop: true,
     delaySpeed: 1700,
   });
   return (
-    <div>
-      <h1>
-        <span>{text}</span>;
-        <Cursor cursorColor="red" />
-      </h1>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden object-cover">
+      <BackgroundCircle />
+      <Image
+        src={ProfilePic}
+        alt="Picture or owner"
+        className="relative rounded-full h-72 w-72 mx-auto "
+      />
+      <div>
+        <h2>
+          {text}
+          <Cursor cursorColor="pink" />
+        </h2>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-cyan-800 text-white">
+    <div className="bg-cyan-800 text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Gunter's Portfolio</title>
       </Head>
       <main>{/* <h1 className="text-blue-800">Web Portfolio</h1> */}</main>
       <Header />
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
       {/* About */}
