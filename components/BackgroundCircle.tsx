9fc8543e132f0b721b45b9b78c673d6941ea7850
haluskua <haluskua.one@gmail.com>
2022-11-22 23:02:49 +1300

    added links and tracking

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -6,10 +6,10 @@ function BackgroundCircle({}: Props) {
   return (
     <div className="relative flex justify-center items-center">
       <div className="border border-red-300 rounded-full h-[200px] w-[200px] mt-52 animate-ping absolute opacity-30" />
-      <div className="border absolute border-red-300 rounded-full h-[300px] w-[300px] mt-52 animate-pulse " />
+      {/* <div className="border absolute border-red-300 rounded-full h-[300px] w-[300px] mt-52 animate-pulse " /> */}
       <div className="border absolute border-red-300 rounded-full h-[00px] w-[700px] mt-52 animate-pulse " />
       <div className="border absolute border-red-300 rounded-full h-[800px] w-[800px] mt-52 border-[#f7abba opacity-20 animate-pulse " />
-      <div className="border absolute border-red-300 rounded-full h-[1000px] w-[1000px] mt-52 border-[#f7abba opacity-20 " />
+      <div className="border absolute border-red-300 rounded-full h-[950px] w-[1000px] mt-52 border-[#f7abba opacity-20 " />
     </div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircle from "./BackgroundCircle";
 import Image from "next/image";
-import ProfilePic from "../public/images/0.jpg";
+import ProfilePic from "../public/images/dad.jpg";
+import Link from "next/link";
 
 type Props = {};
 
@@ -17,18 +18,35 @@ const Hero = (props: Props) => {
     delaySpeed: 1700,
   });
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden object-cover">
+    <div className="h-screen flex flex-col space-y-4 items-center justify-center text-center overflow-hidden ">
       <BackgroundCircle />
       <Image
         src={ProfilePic}
         alt="Picture or owner"
-        className="relative rounded-full h-72 w-72 mx-auto "
+        className="relative rounded-full h-48 w-48 mx-auto object-cover"
       />
-      <div>
-        <h2>
-          {text}
-          <Cursor cursorColor="pink" />
+      <div className="z-20">
+        <h2 className="text-sm uppercase pb-2 text-[#f4faff] tracking-[14px] py-4">
+          Frontend Developer
         </h2>
+        <h1 className="text-1xl lg:text-2xl font-semibold mb-2">
+          <span className="mr-3 tracking-[1px]">{text}</span>
+          <Cursor cursorColor="pink" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Projects</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
