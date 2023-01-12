7a70a26c812e96fc419e54d39a41c9f66b8d08bc
haluskua <haluskua.one@gmail.com>
2023-01-12 13:07:10 +1300

    added link to sticky nav email tab

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import Link from "next/link";
 import React, { ReactElement } from "react";
 import { SocialIcon } from "react-social-icons";
 
@@ -30,22 +31,24 @@ export default function Header({}: Props): ReactElement {
           bgColor="transparent"
         />
       </motion.div>
-      <motion.div
-        initial={{ x: 500, opacity: 0, scale: 0 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1.5 }}
-        className="flex flex-row items-center text-[#fff9f9] cursor-pointer rounded-full  p-2 bg-[#54dd67cc] hover:bg-[#f7abba] hover:text-white z-999999"
-      >
-        <SocialIcon
-          className="cursor-pointer  h-5 w-5 mr-3 hover:animate-spin"
-          network="email"
-          fgColor="#e9d0d5ab"
-          bgColor="transparent"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm hover:text-white ">
-          Get In Touch
-        </p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          initial={{ x: 500, opacity: 0, scale: 0 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-row items-center text-[#fff9f9] cursor-pointer rounded-full  p-2 bg-[#54dd67cc] hover:bg-[#f7abba] hover:text-white z-999999"
+        >
+          <SocialIcon
+            className="cursor-pointer  h-5 w-5 mr-3 hover:animate-spin"
+            network="email"
+            fgColor="#e9d0d5ab"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm hover:text-white ">
+            Get In Touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 }
