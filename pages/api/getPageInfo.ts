37bee519291c3b917042e0a1f31f975c7fe62628
haluskua <haluskua.one@gmail.com>
2023-01-15 02:22:15 +1300

    Created End-Points with sanity

---
+++ pages/api/getPageInfo.ts
@@ -0,0 +1,25 @@
+// import { NextApiResponse } from "next";
+//Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { sanityClient } from "../../sanity";
+import { PageInfo } from "../../typings";
+
+// Creating groq query
+
+const query = groq`
+*[_type == "pageInfo"] [0]
+}`;
+
+type Data = {
+  pageInfo: PageInfo[];
+};
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const pageInfo: PageInfo[] = await sanityClient.fetch(query);
+
+  res.status(200).json({ pageInfo });
+}
---
+++ pages/api/getProjects.ts
@@ -0,0 +1,27 @@
+// import { NextApiResponse } from "next";
+//Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { sanityClient } from "../../sanity";
+import { Project } from "../../typings";
+
+// Creating groq query
+
+const query = groq`
+*[_type == "project"] {
+...,
+technologies[]->
+}`;
+
+type Data = {
+  projects: Project[];
+};
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const projects: Project[] = await sanityClient.fetch(query);
+
+  res.status(200).json({ projects });
+}
